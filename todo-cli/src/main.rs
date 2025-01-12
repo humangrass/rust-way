68296642f7187ecae708376b067b2abb80d148f3
Alex Mikhankov <human.grass@gmail.com>
2025-01-12 20:45:37 +0300

    fix todo cli

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -17,16 +17,16 @@ fn main() {
             Task::start(sub_matches);
             println!("Start task");
         }
-        Some((cli::COMMAND_DELETE, sub_matches)) => {
+        Some((cli::COMMAND_DELETE, _sub_matches)) => {
             println!("Delete task");
         }
-        Some((cli::COMMAND_LIST, sub_matches)) => {
+        Some((cli::COMMAND_LIST, _sub_matches)) => {
             Task::show_list();
         }
-        Some((cli::COMMAND_EDIT, sub_matches)) => {
+        Some((cli::COMMAND_EDIT, _sub_matches)) => {
             println!("Edite task");
         }
-        Some((cli::COMMAND_COMPLETE, sub_matches)) => {
+        Some((cli::COMMAND_COMPLETE, _sub_matches)) => {
             println!("Complete task");
         }
         _ => {
